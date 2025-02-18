22fc6371568546e91d559e974dbf6cf3f4543fe0
TorqueReborn <torquereborn@gmail.com>
2025-02-19 00:01:35 +0530

    Triangle

---
+++ C/Pattern/Triangle/triangle.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main(){
+    int i;
+    int j;
+    int num;
+
+    printf("Enter num: ");
+    scanf("%d", &num);
+
+    for(i=1; i<=num; i++){
+        for(j=1; j<=num; j++){
+            if(
+                (i == num) ||
+                (j == num) ||
+                (i+j == num + 1)
+            ){
+                printf(" *");
+            }else {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+}
