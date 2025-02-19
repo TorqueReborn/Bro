afcc9a7ec7c2f42e2360044a4fd71c4707cf897b
TorqueReborn <torquereborn@gmail.com>
2025-02-19 12:56:14 +0530

    Add implementations for number square patterns in C

---
+++ C/Pattern/Number Square/number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main(){
+
+    int i;
+    int j;
+    int n = 5;
+
+    for(i=1; i<=n; i++){
+        for(j=1; j<=n; j++){
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+
+}
